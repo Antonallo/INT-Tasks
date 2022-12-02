ebaf1863d2bd4522caa61bdf382192f30cb4c555
Antonallo <100342766+Antonallo@users.noreply.github.com>
2022-12-02 23:24:18 +0200

    AAA

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void chahging_word_2_vowel_letters(string& a, ofstream& k)
+{
+    string glasn = "аоиыуэяюёеАОИЫУЭЯЮЁЕ";
+    bool f = false;
+    string itog;
+    for (int j = 0; j < a.size() - 1; j++)
+    {
+        for (int i = 0; i < glasn.size(); i++)
+        {
+            if (a[j] == glasn[i] && tolower(a[j]) == tolower(a[j + 1]))
+            {
+                a[j] = toupper(a[j]);
+                a[j + 1] = toupper(a[j + 1]);
+                itog += a[j];
+                f = true;
+            }
+            
+        }
+    }
+    if (f == true)
+        k << a << "(" << itog << ")" << " ";
+    else
+        k << a << " ";
+}
+
+int main()
+{
+
+
+    setlocale(LC_ALL, "rus");
+    string s;
+    ifstream in("in.txt");
+    ofstream out("out.txt");
+    while (!in.eof())
+    {
+        in >> s;
+        chahging_word_2_vowel_letters(s, out);     
+    }
+}
+
+
---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+
+class Vector
+{
+private:
+	int t;
+
+public:
+	int* mas = new int[t];
+
+	Vector(int a)
+	{
+		t = a;
+	}
+
+	~Vector()
+	{
+		delete[] mas;
+	}
+
+	int value(int a)
+	{
+		return mas[a];
+	}
+
+	void push_back(int b)
+	{	
+		for (int i = 0; i < t - 1; i++)
+			mas[i] = mas[i + 1];
+
+		mas[t - 1] = b;
+	}
+};
+
+
+int main()
+{	
+	Vector* vector1 = new Vector (9);
+	vector1->push_back(222);
+	std::cout << vector1->value(8);
+}
