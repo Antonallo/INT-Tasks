e9ee6c222ad6cc773915445275a781de53927824
Antonallo <100342766+Antonallo@users.noreply.github.com>
2022-12-20 16:26:01 +0200

    a

---
+++ ConsoleApplication3/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <bitset>
+std::bitset<8> compression(unsigned int a, unsigned int b)
+{
+    std::bitset<8> b1(a);
+    std::bitset<8> b2(b);
+    std::bitset<8> b3 = (b1 << 4) | b2;
+    return b3;
+}
+
+void extraction(std::bitset<8> a)
+{
+    unsigned int b = (a >> 4).to_ulong();
+    unsigned int c = ((a << 4) >> 4).to_ulong();
+    std::cout << b << std::endl;
+    std::cout << c;
+}
+int main()
+{
+    int a = 5;
+    int b = 10;
+    std::cout <<compression(a, b) << std::endl;
+    extraction(compression(a, b));
+}
